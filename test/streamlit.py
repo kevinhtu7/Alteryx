0cf9c7bf84cdcf4ea096035d633066cf37f60323
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-05-28 21:57:35 -0700

    Update streamlit.py

--- test/streamlit.py
+++ test/streamlit.py
@@ -2,14 +2,14 @@ from main import ChatBot
 import streamlit as st
 
 bot = ChatBot()
-
+    
 st.set_page_config(page_title="Random Fortune Telling Bot")
 with st.sidebar:
     st.title('Random Fortune Telling Bot')
 
 # Function for generating LLM response
 def generate_response(input):
-    result = bot.get_response(input)
+    result = bot.rag_chain.invoke(input)
     return result
 
 # Store LLM generated responses
@@ -18,23 +18,20 @@ if "messages" not in st.session_state.keys():
 
 # Display chat messages
 for message in st.session_state.messages:
-    if message["role"] == "assistant":
-        with st.chat_message(message["role"]):
-            st.write(message["content"])
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
 
 # User-provided prompt
 if input := st.chat_input():
-    # Generate a new response if the last message is not from the assistant
-    if st.session_state.messages[-1]["role"] == "user":
-        with st.chat_message("assistant"):
-            with st.spinner("Getting your answer from mystery stuff..."):
-                response = generate_response(input)
-                st.write(response)
-                message = {"role": "assistant", "content": response}
-                st.session_state.messages.append(message)
-                
-    # Append user input to session state without displaying it
     st.session_state.messages.append({"role": "user", "content": input})
-    
-    # Remove the user's input from session state to prevent it from being displayed
-    st.session_state.messages = [msg for msg in st.session_state.messages if msg["role"] != "user"]
+    with st.chat_message("user"):
+        st.write(input)
+
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Getting your answer from mystery stuff.."):
+            response = generate_response(input) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
