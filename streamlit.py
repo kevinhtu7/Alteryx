2853088e68807f3f1a1e305b76fc0c8f15b178ab
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-05-28 21:27:02 -0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,14 +2,14 @@ from main import ChatBot
 import streamlit as st
 
 bot = ChatBot()
-    
+
 st.set_page_config(page_title="Random Fortune Telling Bot")
 with st.sidebar:
     st.title('Random Fortune Telling Bot')
 
 # Function for generating LLM response
 def generate_response(input):
-    result = bot.rag_chain.invoke(input)
+    result = bot.get_response(input)
     return result
 
 # Store LLM generated responses
@@ -18,20 +18,17 @@ if "messages" not in st.session_state.keys():
 
 # Display chat messages
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    if message["role"] == "assistant":
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
 # User-provided prompt
 if input := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": input})
-    with st.chat_message("user"):
-        st.write(input)
-
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Getting your answer from mystery stuff.."):
-            response = generate_response(input) 
-            st.write(response) 
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+    # Generate a new response if the last message is not from the assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Getting your answer from mystery stuff..."):
+                response = generate_response(input)
+                st.write(response)
+                message = {"role": "assistant", "content": response}
+                st.session_state.messages.append(message)
