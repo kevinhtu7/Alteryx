3c4e703148f73c94b6356fb75b9e93b0798cf678
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-05-28 18:15:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,44 +15,33 @@ class ChatBot():
   load_dotenv()
   loader = TextLoader('./horoscope.txt')
   documents = loader.load()
-
-  loader = TextLoader('./horoscope.txt')
-  documents = loader.load()
   text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=4)
   docs = text_splitter.split_documents(documents)
+
   embeddings = HuggingFaceEmbeddings()
 
-  # Initialize Pinecone client
   pinecone.init(
       api_key= os.getenv('PINECONE_API_KEY'),
       environment='gcp-starter'
   )
 
-# Define Index Name
   index_name = "langchain-demo"
 
-# Checking Index
   if index_name not in pinecone.list_indexes():
-  # Create new Index
     pinecone.create_index(name=index_name, metric="cosine", dimension=768)
     docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
   else:
-  # Link to the existing index
     docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
-# Define the repo ID and connect to Mixtral model on Huggingface
   repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
   llm = HuggingFaceHub(
-    repo_id=repo_id, 
-    model_kwargs={"temperature": 0.8, "top_k": 50}, 
-    huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
+      repo_id=repo_id, model_kwargs={"temperature": 0.8, "top_p": 0.8, "top_k": 50}, huggingfacehub_api_token=os.getenv('HUGGINGFACE_API_KEY')
   )
 
   template = """
-  You are a fortune teller. These Human will ask you a questions about their life. 
-  Use following piece of context to answer the question. 
+  You are a seer. These Human will ask you a questions about their life. Use following piece of context to answer the question. 
   If you don't know the answer, just say you don't know. 
-  Keep the answer within 2 sentences and concise.
+  You answer with short and concise answer, no longer than2 sentences.
 
   Context: {context}
   Question: {question}
@@ -60,10 +49,7 @@ class ChatBot():
 
   """
 
-  prompt = PromptTemplate(
-    template=template, 
-    input_variables=["context", "question"]
-  )
+  prompt = PromptTemplate(template=template, input_variables=["context", "question"])
 
   rag_chain = (
     {"context": docsearch.as_retriever(),  "question": RunnablePassthrough()} 
