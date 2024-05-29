d1ff748933affbdc8ab639e6f9c806a526b53e5c
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-05-28 21:50:49 -0700

    Update main.py

--- test/main.py
+++ test/main.py
@@ -9,6 +9,7 @@ import os
 
 class ChatBot():
   load_dotenv()
+  current_directory = os.path.dirname(os.path.abspath(__file__))
   file_path = os.path.join(current_directory, 'horoscope.txt')
   loader = TextLoader(file_path)
   documents = loader.load()
