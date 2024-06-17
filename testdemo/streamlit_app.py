0e00023a0abc2640a65de08e74617495c93367aa
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-06-17 16:58:59 -0700

    Update streamlit_app.py

--- testdemo/streamlit_app.py
+++ testdemo/streamlit_app.py
@@ -14,32 +14,36 @@ role = st.radio(
     ["Admin", "General"],
     captions = ["All access", "Restricted Access"])
 
+if role == "Admin":
+    
 
-# Function for generating LLM response
-def generate_response(input):
-    result = bot.rag_chain.invoke(input)
-    return result
+    # Function for generating LLM response
+    def generate_response(input):
+        result = bot.rag_chain.invoke(input)
+        return result
 
-# Store LLM generated responses
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Welcome, what can I help you with?"}]
+    # Store LLM generated responses
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "Welcome, what can I help you with?"}]
 
-# Display chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    # Display chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-# User-provided prompt
-if input := st.chat_input():
-    st.session_state.messages.append({"role": "user", "content": input})
-    with st.chat_message("user"):
-        st.write(input)
+    # User-provided prompt
+    if input := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": input})
+        with st.chat_message("user"):
+            st.write(input)
 
-# Generate a new response if last message is not from assistant
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Grabbing your answer from database..."):
-            response = generate_response(input) 
-            st.write(response) 
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+    # Generate a new response if the last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Grabbing your answer from database..."):
+                response = generate_response(input)
+                st.write(response)
+            message = {"role": "assistant", "content": response}
+            st.session_state.messages.append(message)
+            
+else st.write("You are not an admin")
