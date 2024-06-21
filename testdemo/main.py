89b3269c896cd89262e31949087a2a3dd89ad566
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-06-20 18:23:52 -0700

    Update main.py

--- testdemo/main.py
+++ testdemo/main.py
@@ -47,7 +47,7 @@ class ChatBot():
     def get_context_from_collection(self):
         # Extract context from the collection
         documents = self.collection.get()
-        context = documents["documents"][0] # Get the first document, This Line allows the bot to only use the first document in the collection
+        context = documents["documents"][1] # Get the first document, This Line allows the bot to only use the first document in the collection
         #BELOW IS THE OLD SEARCH METHOD. DOES NOT WORK BECAUSE IT LOADS EVERYTHING. WAS FINE WITH 1 DOC BUT NOT WITH MULTIPLE.
         #context = " ".join([doc["content"] for doc in documents["documents"]])
         return context
@@ -57,8 +57,8 @@ class ChatBot():
     def setup_langchain(self):
         template = """
         You are an informational chatbot. These employees will ask you questions about company data and meeting information. Use the following piece of context to answer the question.
-        If you don't know the answer, just say you don't know.
-        You answer with short and concise answers, no longer than 2 sentences.
+        If you don't know the answer, just say you don't know. Please provide the file used for context.
+        # You answer with short and concise answers, no longer than 2 sentences.
 
         Context: {context}
         Question: {question}
