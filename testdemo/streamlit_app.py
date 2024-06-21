a5a843f1c41c49000be3a6a2e8ddbaf13f86a8cb
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-06-20 19:44:12 -0700

    Update streamlit_app.py

--- testdemo/streamlit_app.py
+++ testdemo/streamlit_app.py
@@ -9,11 +9,11 @@ with st.sidebar:
 
 role = st.radio(
     "What's your role",
-    ["Admin", "General"],
-    format_func=lambda x: "All access" if x == "Admin" else "Restricted Access"
+    ["Executive Access", "General Access"],
+    format_func=lambda x: "All access" if x == "Executive Access" else "General Access"
 )
 
-if role == "Admin":
+if role == "Executive Access":
     # Function for generating LLM response
     def generate_response(input_dict):
         result = bot.rag_chain.invoke(input_dict)
@@ -44,4 +44,32 @@ if role == "Admin":
             message = {"role": "assistant", "content": response}
             st.session_state.messages.append(message)
 else:
-    st.write("You are not an admin")
+     # Function for generating LLM response
+    def generate_response(input_dict):
+        result = bot.rag_chain.invoke(input_dict)
+        return result
+
+    # Store LLM generated responses
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": "Welcome, what can I help you with?"}]
+
+    # Display chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    # User-provided prompt
+    if input := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": input})
+        with st.chat_message("user"):
+            st.write(input)
+
+        # Generate a new response
+        context = bot.get_context_from_collection(input, access_role=role)
+        input_dict = {"context": context, "question": input}
+        with st.chat_message("assistant"):
+            with st.spinner("Grabbing your answer from database..."):
+                response = generate_response(input_dict)
+                st.write(response)
+            message = {"role": "assistant", "content": response}
+            st.session_state.messages.append(message)
