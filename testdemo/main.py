609909eb3eac67627ebbbf705e5a085edb424e60
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-06-20 18:43:43 -0700

    Update main.py

--- testdemo/main.py
+++ testdemo/main.py
@@ -47,7 +47,7 @@ class ChatBot():
     def get_context_from_collection(self):
         # Extract context from the collection
         documents = self.collection.get()
-        context = documents["documents"][2] # Get the first document, This Line allows the bot to only use the first document in the collection
+        context = documents["documents"][16] # Get the first document, This Line allows the bot to only use the first document in the collection
         #BELOW IS THE OLD SEARCH METHOD. DOES NOT WORK BECAUSE IT LOADS EVERYTHING. WAS FINE WITH 1 DOC BUT NOT WITH MULTIPLE.
         #context = " ".join([doc["content"] for doc in documents["documents"]])
         return context
