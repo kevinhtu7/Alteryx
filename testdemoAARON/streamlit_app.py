43c4f63ba604d8f984fe9d9656bb6dc2725d5fb0
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-07-01 22:12:39 -0700

    Update streamlit_app.py

--- testdemoAARON/streamlit_app.py
+++ testdemoAARON/streamlit_app.py
@@ -1,24 +1,22 @@
-# streamlit_app.py
 from main import ChatBot
 import streamlit as st
 
 bot = ChatBot()
-
+    
 st.set_page_config(page_title="Meeting Information Bot")
 with st.sidebar:
     st.title('Meeting Information Bot')
 
-role = st.radio(
-    "What's your role",
-    ["General Access", "Executive Access"],
-    format_func=lambda x: "Executive Access" if x == "Executive Access" else "General Access"
-)
+# Function for generating LLM response
+def generate_response(input):
+    result = bot.rag_chain.invoke(input)
+    return result
 
-# Store and display chat messages
-if "messages" not in st.session_state:
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "Welcome, what can I help you with?"}]
-    st.session_state.context_history = []
 
+# Display chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
@@ -29,19 +27,11 @@ if input := st.chat_input():
     with st.chat_message("user"):
         st.write(input)
 
-    # Debugging output
-    st.write("Input received:", input)
-
-    # Retrieve context from the database
-    context = bot.get_context_from_collection(input, access_role=role)
-    st.write("Context retrieved:", context)
-    st.session_state.context_history.append(context)
-
-    # Generate a new response
-    input_dict = {"context": context, "question": input}
+# Generate a new response if last message is not from assistant
+if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Grabbing your answer from database..."):
-            response = bot.generate_response(input_dict)
-            st.write("Response generated:", response)
-        message = {"role": "assistant", "content": response}
-        st.session_state.messages.append(message)
+            response = generate_response(input) 
+            st.write(response) 
+    message = {"role": "assistant", "content": response}
+    st.session_state.messages.append(message)
