e8a032b453c252d22940e61e4f0ce4e4e2cc5c30
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-07-01 21:50:51 -0700

    Create streamlit_app.py

---
+++ testdemoAARON/streamlit_app.py
@@ -0,0 +1,43 @@
+# streamlit_app.py
+from main import ChatBot
+import streamlit as st
+
+bot = ChatBot()
+
+st.set_page_config(page_title="Meeting Information Bot")
+with st.sidebar:
+    st.title('Meeting Information Bot')
+
+role = st.radio(
+    "What's your role",
+    ["General Access", "Executive Access"],
+    format_func=lambda x: "Executive Access" if x == "Executive Access" else "General Access"
+)
+
+# Store and display chat messages
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Welcome, what can I help you with?"}]
+    st.session_state.context_history = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# User-provided prompt
+if input := st.chat_input():
+    st.session_state.messages.append({"role": "user", "content": input})
+    with st.chat_message("user"):
+        st.write(input)
+
+    # Retrieve context from the database
+    context = bot.get_context_from_collection(input, access_role=role)
+    st.session_state.context_history.append(context)
+
+    # Generate a new response
+    input_dict = {"context": context, "question": input}
+    with st.chat_message("assistant"):
+        with st.spinner("Grabbing your answer from database..."):
+            response = bot.generate_response(input_dict)
+            st.write(response)
+        message = {"role": "assistant", "content": response}
+        st.session_state.messages.append(message)
