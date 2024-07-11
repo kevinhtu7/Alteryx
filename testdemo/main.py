ed20b78feb86eb91ddf372253cd23546299cb982
kevinhtu7 <144483932+kevinhtu7@users.noreply.github.com>
2024-07-10 17:52:32 -0700

    Update main.py

--- testdemo/main.py
+++ testdemo/main.py
@@ -52,13 +52,16 @@ class ChatBot():
         #                                         )
 
         elif access_role == "Executive Access":
+            access_text = "{"+ "access_role: "+ "General Access"+ "}" + "," +  "{"+"access_role: " + access_role
             documents = self.collection.query(query_texts=[input],
                                               n_results=3,
                                               where={
-                                              "$or": [{"access_role": "General Access"}, {"access_role": access_role}
-                                                ]
+                                              # "$or": [{"access_role": "General Access"}, {"access_role": access_role}
+                                                "$or": [access_text]
                                               }
                                                 )
+
+        
         
         for document in documents["documents"]:
             context = document
