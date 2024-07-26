1028b63c9cd9d9fd7769f9bbaa9010836a472091
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-07-25 22:59:13 -0700

    Update streamlit_app.py

--- testdemoAARON/streamlit_app.py
+++ testdemoAARON/streamlit_app.py
@@ -94,7 +94,8 @@ def main():
                 st.session_state.disable_inputs = True
                 st.success(f"Welcome {username}! Your role is {role} with {access_levels} access.")
                 run_app(access_levels)
-                st.experimental_rerun()  # Rerun the app to update the UI
+                #st.experimental_rerun()  # Rerun the app to update the UI
+                st.experimental_set_query_params(rerun="true")  # Rerun the app to update the UI
             else:
                 st.error("Invalid username or password")
             #st.session_state.username = ""
