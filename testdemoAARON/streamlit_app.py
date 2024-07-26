c33859684c6330db82363ba2b69fa0afb9df6313
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-07-25 22:45:41 -0700

    Update streamlit_app.py

--- testdemoAARON/streamlit_app.py
+++ testdemoAARON/streamlit_app.py
@@ -91,6 +91,7 @@ def main():
                 st.session_state.logged_in = True
                 st.session_state.username = username
                 st.session_state.access_levels = access_levels
+                st.session_state.disable_inputs = True
                 st.success(f"Welcome {username}! Your role is {role} with {access_levels} access.")
                 run_app(access_levels)
             else:
