f975eb75df266b3e7fb9a138cf163bee2b1ae9b4
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-07-26 00:24:33 -0700

    Update streamlit_app.py

--- testdemoAARON/streamlit_app.py
+++ testdemoAARON/streamlit_app.py
@@ -92,9 +92,10 @@ def main():
                 st.session_state.logged_in = True
                 st.session_state.username = username
                 st.session_state.access_levels = access_levels
-                st.success(f"Welcome {username}! Your role is {role} with {access_levels} access.")
+                #st.success(f"Welcome {username}! Your role is {role} with {access_levels} access.")
                 run_app(st.session_state.access_levels)
                 st.rerun()
+                st.success(f"Welcome {username}! Your role is {role} with {access_levels} access.")
                 #run_app(access_levels)
             else:
                 st.error("Invalid username or password")
