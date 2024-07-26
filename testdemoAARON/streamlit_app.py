eb08c11dfc3d7bdfd99b0dc8769c398810b0e23f
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-07-25 22:32:45 -0700

    Update streamlit_app.py

--- testdemoAARON/streamlit_app.py
+++ testdemoAARON/streamlit_app.py
@@ -95,8 +95,6 @@ def main():
                 run_app(access_levels)
             else:
                 st.error("Invalid username or password")
-            st.session_state.username = ""
-            st.session_state.password = ""
     else:
         run_app(st.session_state.access_levels)
 
