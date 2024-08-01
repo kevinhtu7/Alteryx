c16da45075a4db9cef7b9dde113981890fb77296
asornbor <144483884+asornbor@users.noreply.github.com>
2024-08-01 16:54:57 -0700

    Update main.py

--- testdemoAARON/main.py
+++ testdemoAARON/main.py
@@ -86,7 +86,7 @@ class ChatBot():
 
     def create_openai_chat_model(self):
         return lambda prompt: openai.chat.completions.create(
-            model="gpt-4-turbo",  #model may need updating
+            model="gpt-4o-mini-2024-07-18",  #model may need updating
             messages=[{"role": "system", "content": "You are an informational chatbot. Use the following piece of context to answer the question. If you don't know the answer, simply state 'You do not have the required level of access.'"}, {"role": "user", "content": prompt}],
             temperature=0.8
         )
