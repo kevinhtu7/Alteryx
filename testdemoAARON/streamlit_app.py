19506abd8622fcfd1f9ff423fc3799a6a2a6f13a
Mason Dosher <131201109+AmongChevelle@users.noreply.github.com>
2024-08-08 20:17:02 -0700

    Update streamlit_app.py

--- testdemoAARON/streamlit_app.py
+++ testdemoAARON/streamlit_app.py
@@ -179,6 +179,8 @@ def run_app(access_levels):
             with st.chat_message("user"):
                 st.write(input)
 
+            input_dict = {"question": input, "access_levels": access_levels}
+            
             # Retrieve context from the database
             try:
                 #context = bot.get_combined_context(input, access_levels=access_levels)
@@ -195,7 +197,7 @@ def run_app(access_levels):
 
             # Generate a new response
             #input_dict = {"context": context, "question": input}
-            input_dict = {"question": input, "access_levels": access_levels}
+            #input_dict = {"question": input, "access_levels": access_levels}
             with st.chat_message("assistant"):
                 with st.spinner("Grabbing your answer from database..."):
                     response = generate_response(input_dict)
